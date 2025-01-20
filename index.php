eb71f42aa0ec3348029fabd2f437463ca22709cd
Bommi1107 <eddevinith@gmail.com>
2025-01-20 10:43:05 +0530

    Add project files

---
+++ index.php
@@ -0,0 +1,191 @@
+<?php 
+    session_start();
+
+    ?>
+<!DOCTYPE html>
+<html>
+<?php $title="Bloodbank | index page"; ?>
+<?php require 'head.php'; ?>
+<head>
+  <title></title>
+   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/styles.css"> 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  
+</head>
+<body>
+<?php require 'header.php'; ?>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+    </ul>
+  
+  </div>
+</nav>
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/slide-1.jpg" alt="Los Angeles" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="image/cc1.jpg" alt="Chicago" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="image/blog-4.png" alt="New York" width="1100" height="500">
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <div class="container cont">
+
+   <?php require 'message.php'; ?>
+
+        <div class="row justify-content-center">
+            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5 col-xs-6 mb-5" style="width: 60%">
+                <div class="card">
+                    <img src="image/bg.png" class="card-img-top">
+                </div>
+            </div>
+
+            <div class="col-lg-9">
+            <div class="row">
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">A+</div>
+                    <div class="card-body">
+                        If you are A+: You can give blood to A+ and AB+. You can receive blood from A+, A-, O+ and O-
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">A-</div>
+                    <div class="card-body">
+                        If you are A-: You can give blood to A-, A+, AB- and AB+. You can receive blood from A- and O-. 
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">B+</div>
+                    <div class="card-body">
+                         You can give blood to A+ and AB+. You can receive blood from A+, A-, O+ and O-.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">B-</div>
+                    <div class="card-body">
+                        If you are B-: You can give blood to B-, B+, AB+ and AB-, You can receive blood from B- and O-.You can give blood to B+ and AB+.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">AB+</div>
+                    <div class="card-body">
+                         People with AB+ blood can receive red blood cells from any blood type. This means that demand for AB+ can donate with AB only.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">AB-</div>
+                    <div class="card-body"> 
+                         AB- patients can receive red blood cells from all negative blood types.
+                         AB- can give red blood cells to both AB- and AB+ blood types.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">O+</div>
+                    <div class="card-body">
+                        Blood O+ can donate to A+, B+, AB+ and O+ Blood
+                        Group O can donate red blood cells to anybody. It's the universal donor.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">O-</div>
+                    <div class="card-body">
+                        O- can donate to A+, A-, B+, B-, AB+, AB-, O+ and O- Blood
+                        People with O negative blood can only receive red cell donations from O negative donors.
+                    </div>
+                </div>
+            </div>
+            
+            </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6 rounded mb-5">
+
+            </div>
+            <div class="col-lg-6 rounded mb-5">
+                 </div>
+        </div>
+
+        <div class="row mb-5">
+            <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">Health tips</div>
+                <div class="card-body">
+                    <dt>Eat a healthy diet</dt>
+                    <dd>Eat a combination of different foods, including fruit, vegetables, legumes, nuts and whole grains. Adults should eat at least five portions (400g) of fruit and vegetables per day. You can improve your intake of fruits and vegetables by always including veggies in your meal; eating fresh fruit and vegetables as snacks; eating a variety of fruits and vegetables; and eating them in season. By eating healthy, you will reduce your risk of malnutrition and noncommunicable diseases (NCDs) such as diabetes, heart disease, stroke and cancer.</dd>
+                    <dt> Consume less salt and sugar</dt>
+                    <dd>On the other hand, consuming excessive amounts of sugars increases the risk of tooth decay and unhealthy weight gain. In both adults and children, the intake of free sugars should be reduced to less than 10% of total energy intake. This is equivalent to 50g or about 12 teaspoons for an adult. WHO recommends consuming less than 5% of total energy intake for additional health benefits. You can reduce your sugar intake by limiting the consumption of sugary snacks, candies and sugar-sweetened beverages.</dd>
+                    <dt>Be active</dt>
+                    <dd>Physical activity is defined as any bodily movement produced by skeletal muscles that requires energy expenditure. This includes exercise and activities undertaken while working, playing, carrying out household chores, travelling, and engaging in recreational pursuits. The amount of physical activity you need depends on your age group but adults aged 18-64 years should do at least 150 minutes of moderate-intensity physical activity throughout the week. Increase moderate-intensity physical activity to 300 minutes per week for additional health benefits.</dd>
+                    <dt>Don't smoke</dt>
+                    <dd>Smoking tobacco causes NCDs such as lung disease, heart disease and stroke. Tobacco kills not only the direct smokers but even non-smokers through second-hand exposure. Currently, there are around 15.9 million Filipino adults who smoke tobacco but 7 in 10 smokers are interested or plan to quit.
+                    If you are currently a smoker, it's not too late to quit. Once you do, you will experience immediate and long-term health benefits. If you are not a smoker, that’s great! Do not start smoking and fight for your right to breathe tobacco-smoke-free air.</dd>
+                    <dt>Drink only safe water</dt>
+                    <dt>Get tested</dt>
+                    <dt>Follow traffic laws</dt>
+                    <dt>Talk to someone you trust if you're feeling down</dt>
+                    <dt>Clean your hands properly</dt>
+                    <dt>Have regular check-ups</dt>
+                    Visit here to get more health tips.
+                    <a href="https://www.who.int/philippines/news/feature-stories/detail/20-health-tips-for-2020" target="_blank"> World Health Organisation</a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <?php require 'footer.php'; ?>
+
+</body>
+</html>
